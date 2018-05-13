11d963492ba9bc3aeed1f102a93694a6cd1f74e2
Michael Andrés Murcia Contreras <mamurciac@unal.edu.co>
2018-05-12 22:25:00 -0500

    Página de Inicio
    Creación del controlador para inicio de la página

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'inicio/index'
+
   resources :cotizmesexps
   resources :cotizmescams
   resources :cotizesccams
   resources :cotizclomuls
+
+  root :to => 'inicio#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
